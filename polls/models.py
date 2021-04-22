51f982deb0d6fb89f171076eab7e9110824eff53
samir <samir.touira@epitech.eu>
2021-04-22 02:59:12 +0200

    warning issues resolved

--- polls/models.py
+++ polls/models.py
@@ -2,25 +2,30 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    nombre_heure = models.IntegerField(max_length=1000)
+    nombre_heure = models.IntegerField()
 
 class Instructor(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
 
 class Planning(models.Model):
+    id = models.AutoField(primary_key=True)
     student = models.ForeignKey(Student, on_delete=models.CASCADE),
     heure = models.TimeField(auto_now=False, auto_now_add=False),
     date = models.DateField(auto_now=False, auto_now_add=False),
     lieux = models.CharField(max_length=100)
 
 class Scretary(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
 
 class Admin(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
 
