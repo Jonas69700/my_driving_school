1a2db5e166fa566af488c92ad57033a1f5f685b5
samir <samir.touira@epitech.eu>
2021-04-23 14:43:20 +0200

    test forms polls side WIP

--- polls/admin.py
+++ polls/admin.py
@@ -5,8 +5,17 @@ from .models import Student
 from .models import Secretary
 from .models import Instructor
 
+# from forms import PlanningForm
+
+# class PlanningAdmin(admin.ModelAdmin):
+#   list_display = ["location", "date_rdv", "hour", "created", "updated"]
+#   form = PlanningForm
+#   list_filter = ["location", "date_rdv"]
+#   search_fields = ["location", "date_rdv"]
+
 # Register your models here.
 admin.site.register(Planning)
+# , PlanningAdmin)
 admin.site.register(Student)
 admin.site.register(Secretary)
 admin.site.register(Instructor)
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-
+# from forms import PlanningForm
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're the polls index.")
